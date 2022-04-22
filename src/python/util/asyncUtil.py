b3efa9cab4349cf6a769ade80839272adde9fdf5
Chilly712 <chillinthestar@outlook.com>
2022-04-21 21:12:15 -0500

    Update test cases with `aysncio` asynchronous http request

---
+++ src/python/util/asyncUtil.py
@@ -0,0 +1,12 @@
+import aiohttp
+import asyncio
+
+
+async def asyncRequest(session: aiohttp.ClientSession, url: str) -> dict:
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def asyncRequestAll(session: aiohttp.ClientSession, urls: [str]) -> [dict]:
+    asyncTask = [asyncio.ensure_future(asyncRequest(session, url)) for url in urls]
+    return await asyncio.gather(*asyncTask)
