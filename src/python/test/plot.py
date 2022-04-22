aa77997f3ff86df0914357aabff1ceee426c9e4c
CubeSAT_GS <44418247+Chilly712@users.noreply.github.com>
2022-04-22 15:29:30 -0700

    Update plot.py

--- src/python/test/plot.py
+++ src/python/test/plot.py
@@ -5,6 +5,7 @@ import matplotlib
 from matplotlib import pyplot, animation
 from matplotlib.animation import FuncAnimation
 from skyfield.toposlib import wgs84
+import numpy as np
 
 from src.python.service import skyfieldService, tleService
 
@@ -71,11 +72,44 @@ def plotPath() -> FuncAnimation:
     return animation.FuncAnimation(fig, update, frames=len(data) - 1,
                                    init_func=init, interval=1000)
 
+def plotRealTime():
+    satelliteToPlot = getAllSat()[0]
+
+    # plot movement of first satellite
+    fig = pyplot.figure()
+    ax = fig.add_subplot()
+    img = pyplot.imread(urllib.request.urlopen(IMAGE_URL), format='jpg')
+
+    x=satelliteToPlot["lngArray"]
+    y=satelliteToPlot["latArray"]
+
+    # create the first plot
+    point, = ax.plot([x[0]], [y[0]], 'ro')
+    line, = ax.plot(x, y, 'black', label='ground track')
+    ax.legend()
+    ax.set_xlim([-180, 180])
+    ax.set_ylim([-90, 90])
+    ax.imshow(img, origin='upper', extent=[-180, 180, -90, 90], alpha=0.75)
+    ax.annotate(f'. {"Irvine, CA"}', (-117.841132, 33.643831), color='black')
+    # ax.annotate(f'. {"Plano, TX"}', (-96.697442, 32.999553), color='black')
+    ax.annotate(f'. {"Dalian, China"}', (121.6147, 38.9140), color='black')
+    ax.annotate(f'. {"Sydney, AUS"}', (151.2093, -33.8688), color='black')
+    ax.set(xlabel='longitude', ylabel='latitude', title=satelliteToPlot["identifier"])
+
+    # move the point position at every frame
+    def update_point(n, x, y, point):
+        point.set_data(np.array([x[n], y[n]]))
+        return point
+
+    ani=animation.FuncAnimation(fig, update_point, 99, fargs=(x, y, point))
+
+    pyplot.show()
+
 
 if __name__ == "__main__":
     a = plotPath() # flight path of every available satellite
 
     # for every available satellite, an animation of its flight path
-    # b = plotRealTimePath()
+    b = plotRealTime()
 
     pyplot.show()
