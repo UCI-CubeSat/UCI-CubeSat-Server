a09ceb08970968d0ab11368551d5ed95fd13cd9e
Chilly712 <chillinthestar@outlook.com>
2022-05-07 21:32:35 -0700

    Empty commit to trigger Heroku Dyno Update

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from src.python.route.quartWebSocketRoute import webSocketRoute
 from src.python.route.quartServerRoute import serverRoute
 from src.python.config.appConfig import app, quartDebug, quartEnv, quartHost, quartPort
-from src.python.service.tleService import getTwoLineElement
 
 
 _ = [app.register_blueprint(route) for route in [webSocketRoute, serverRoute]]
-_ = getTwoLineElement()
 
 
 if __name__ == "__main__":
